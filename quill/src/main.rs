a78527d8d842d7df152f878b4585e784ccaeed79
Karim <karim.wael@gmail.com>
2022-03-26 17:30:32 +0400

    post done

--- quill/src/main.rs
+++ quill/src/main.rs
@@ -1,3 +1,6 @@
+extern crate quill;
+extern crate diesel;
+use self::quill::*;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
@@ -6,18 +9,15 @@ async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hey sunshine!")
 }
 
-fn make_uuid() -> Result<(), Box<dyn std::error::Error>> {
-    
-        let my_uuid = Uuid::new_v4();
-        println!("{}", my_uuid);
-    Ok(())
-}
 
 async fn create_post(post: String) -> impl Responder {
     let user200: IncomingRequest = serde_json::from_str(&post).unwrap();
     println!("{:?}", user200.data);
     let uuid = Uuid::new_v4();
     println!("{:?}", uuid);
+    let connection = establish_connection();
+    let post = create_Clip(&connection, &uuid.to_string(), &user200.data);
+     println!("\nSaved draft {} with id {}", post.content, post.id);
     HttpResponse::Accepted().body(uuid.to_string())
 }
 #[derive(Serialize, Deserialize, Debug)]
